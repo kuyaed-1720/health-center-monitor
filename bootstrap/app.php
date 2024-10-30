bfdcb1225e7298b02d7f0b4fde5fafccbff6abb6
kuyaed-1720 <kuya.ed20@gmail.com>
2024-10-30 15:25:38 +0800

    System add

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\EnsureUserHasRole;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo('login');
+        $middleware->redirectUsersTo('/');
+        $middleware->alias([
+            'health_worker' => App\Http\Middleware\EnsureUserHasRole::class.':health_worker',
+            'admin' => App\Http\Middleware\EnsureUserHasRole::class.':admin',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
