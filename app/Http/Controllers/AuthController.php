3745667a5776f776cd9134ba6b7f9f3834e674e5
kuyaed-1720 <kuya.ed20@gmail.com>
2024-10-30 15:28:02 +0800

    Add new roles and permissions

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect('/login')->with('success', 'Registration successful!');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            throw ValidationException::withMessages([
+                'email' => [trans('auth.email')],
+            ]);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => [trans('auth.password')],
+            ]);
+        }
+
+        if (Auth::attempt($credentials)) {
+            $user->update(['status' => 'online']);
+            $request->session()->regenerate();
+            session(['username' => $user->first_name . " " . $user->last_name]);
+            session(['email' => $request->email]);
+            session(['role']) => $user->role;
+            return redirect()->intended('/');
+        }
+
+        return back();
+    }
+
+    public function logout()
+    {
+        $user = User::where('email', session('email'))->first();
+        $user->update(['status' => 'offline']);
+        Session::flush();
+        Auth::logout();
+        return redirect('/login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\EnsureUserHasRole;
 
-Route::get('/', function () {
-    return view('welcome');
+// First-timer access
+Route::get('/landing', function () {
+    return view('landing');
+});
+
+// Guest-level access
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [UserController::class, 'showLoginForm'])->name('showLoginForm');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+    Route::get('/register', [UserController::class, 'showRegistrationForm'])->name('showRegistrationForm');
+    Route::post('/register', [UserController::class, 'register'])->name('register');
+});
+
+// User-level access
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [UserController::class, 'welcome']);
+    Route::get('/welcome', [UserController::class, 'welcome'])->name('welcome');
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+    Route::get('/calendar', [EventController::class, 'showCalendar'])->name('calendar');
+    Route::get('/events', [EventController::class, 'getEvents']);
+    Route::get('/tasks', [EventController::class, 'getTasks']);
+    Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
+    Route::post('/events', [EventController::class, 'store'])->name('events.store');
+});
+
+// Admin-level access
+Route::middleware(['admin'])->group(function () {
+    Route::get('/users', function() {
+        return view('users.index');
+    });
 });
